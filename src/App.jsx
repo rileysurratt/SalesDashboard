6f1c5b3bcb67ea6e22bb26f9256ea77d341622aa
rileysurratt <rileysurratt13@gmail.com>
2024-05-20 20:11:35 -0400

    created drawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from 'react'
 import './App.css'
+import SideBar from './components/layout/SideBar'
 
 function App() {
 
   useEffect(() => {
-
+    const fetchData = async () => {
+      const response = await fetch('./MockData.json');
+      const result = await response.json();
+      console.log(result)
+    }
+    fetchData();
   }, [])
 
   return (
     <>
-
+    <SideBar />
     </>
   )
 }
