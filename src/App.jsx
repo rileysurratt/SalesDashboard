d3b3b45732645f28a4c2aab034b74b8a77c31495
rileysurratt <rileysurratt13@gmail.com>
2024-05-29 20:54:25 -0400

    finished basic layout, sidebar and main dashboard are up and running

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import './App.css'
+import Grid from '@mui/material/Grid';
 import SideBar from './components/layout/SideBar'
+import Dashboard from './components/layout/Dashboard'
 
 function App() {
 
@@ -15,7 +17,29 @@ function App() {
 
   return (
     <>
-    <SideBar />
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <SideBar />
+      </Grid>
+      <Grid item xs={12}>
+        <Dashboard />
+      </Grid>
+      <Grid item xs={4}>
+        {/* Sales */}
+      </Grid>
+      <Grid item xs={4}>
+        {/* Clients */}
+      </Grid>
+      <Grid item xs={4}>
+        {/* Traffic */}
+      </Grid>
+      <Grid item xs={12}>
+        {/* Revenue */}
+      </Grid>
+      <Grid item xs={12}>
+        {/* Transactions */}
+      </Grid>
+    </Grid>
     </>
   )
 }
---
+++ src/components/layout/Dashboard.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import { CardContent } from '@mui/material';
+
+
+function Dashboard() {
+
+    return (
+        <>
+        {/* <Card className="dashboardContainer" sx={{ minWidth: 275 }}>
+            <CardContent>
+            <LightModeOutlinedIcon />
+            <Typography variant="h2" component={'div'}>DASHBOARD</Typography>
+            <Typography variant="h4" component={'div'}>Welcome to your dashboard</Typography>
+            </CardContent>
+        </Card> */}
+        </>
+    )
+}
+
+export default Dashboard
